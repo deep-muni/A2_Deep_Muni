b359971e58b785174be7088cb8bedc00104d6e6e
Deep Muni <deep.muni94@gmail.com>
2020-06-06 20:49:48 -0300

    First production build

---
+++ reactapp/build/precache-manifest.883d5dcc474273d8fce83795bcf0095a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d90b9cf6135e2c10ed7588e2d766e287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a8b967e37da69a535b0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3805b02eb4036d9f3fb9",
+    "url": "/static/js/2.e7402e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7402e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8b967e37da69a535b0",
+    "url": "/static/js/main.6ab9492f.chunk.js"
+  },
+  {
+    "revision": "60949e83de0aeb529ab1",
+    "url": "/static/js/runtime-main.25c7e1d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
