85290ce807b5ff9cee0e588854bae49d5f15442d
Deep Muni <deep.muni94@gmail.com>
2020-06-08 03:01:47 -0300

    Registration page 90% complete

--- reactapp/src/components/register/Register.js
+++ reactapp/src/components/register/Register.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import './Register.css'
+import {Link} from "react-router-dom";
 
 class Register extends Component {
 
@@ -7,12 +8,17 @@ class Register extends Component {
         super(props);
 
         this.state = {
-            un_err: false,
-            fn_err: false,
-            ln_err: false,
-            dob_err: false,
-            pass_err: false,
-            cp_err: false
+            validation: {
+                un_err: false,
+                fn_err: false,
+                ln_err: false,
+                dob_err: false,
+                pass_err: false,
+                cp_err: false,
+            },
+            error: false,
+            filled: false,
+            initial: true
         };
 
         this.validate = this.validate.bind(this);
@@ -23,104 +29,182 @@ class Register extends Component {
         switch(e.target.id) {
             case 'uname':
                 if(e.target.value === ''){
-                    this.setState({un_err: true});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            un_err: true,
+                        })
+                    });
                 }else{
-                    this.setState({un_err: false});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            un_err: false,
+                        })
+                    });
                 }
                 break;
             case 'fname':
                 if(e.target.value === ''){
-                    this.setState({fn_err: true});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            fn_err: true,
+                        })
+                    });
                 }else{
-                    this.setState({fn_err: false});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            fn_err: false,
+                        })
+                    });
                 }
                 break;
             case 'lname':
                 if(e.target.value === ''){
-                    this.setState({ln_err: true});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            ln_err: true,
+                        })
+                    });
                 }else{
-                    this.setState({ln_err: false});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            ln_err: false,
+                        })
+                    });
                 }
                 break;
             case 'dob':
                 if(e.target.value === ''){
-                    this.setState({dob_err: true});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            dob_err: true,
+                        })
+                    });
                 }else{
-                    this.setState({dob_err: false});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            dob_err: false,
+                        })
+                    });
                 }
                 break;
             case 'pass':
                 if(e.target.value === ''){
-                    this.setState({pass_err: true});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            pass_err: true,
+                        })
+                    });
                 }else{
-                    this.setState({pass_err: false});
-                }
-                if(e.target.value === document.getElementById('cpass').value){
-                    this.setState({cp_err: false});
-                }else{
-                    this.setState({cp_err: true});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            pass_err: false,
+                        })
+                    });
                 }
                 break;
             case 'cpass':
                 if(e.target.value === document.getElementById('pass').value){
-                    this.setState({cp_err: false});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            cp_err: false,
+                        })
+                    });
                 }else{
-                    this.setState({cp_err: true});
+                    this.setState({
+                        validation: Object.assign({}, this.state.validation, {
+                            cp_err: true,
+                        })
+                    });
                 }
                 break;
             default:
                 break;
         }
+        this.setState({
+            initial: false
+        });
     }
 
     register(){
-        const hasFalseKeys = Object.keys(this.state).some(k => !this.state[k]);
-        console.log(hasFalseKeys);
+        const hasError = Object.keys(this.state.validation).some(val => this.state.validation[val]);
+        if(hasError){
+            this.setState({
+                error: true
+            });
+        }else{
+            if(this.state.initial){
+                this.setState({
+                    filled: true
+                });
+            }else{
+                this.props.history.push('/login');
+            }
+        }
+        setTimeout(() => {
+            this.setState({
+                error: false,
+                filled: false
+            });
+        },2500);
     }
 
     render() {
         return (
-            <div className="form_area">
-                <form>
-                    <div className="input-section">
-                        <label htmlFor="uname">User Name</label>
-                        <input type="text" id="uname" placeholder="User Name" className="inp" onBlur={this.validate}/>
-                        {this.state.un_err ? <div className="error">User Name is required</div> : null}
-                    </div>
+            <div className="wrapper">
+                <div className={this.state.error ? "form_area onerror" : "form_area"}>
+                    <form>
+                        <div className="input-section">
+                            <label htmlFor="uname">User Name</label>
+                            <input type="text" id="uname" className="inp"
+                                   onBlur={this.validate} onChange={this.validate}/>
+                            {this.state.validation.un_err ? <div className="error">User Name is required</div> : null}
+                        </div>
 
-                    <div className="input-section">
-                        <label htmlFor="fname">First Name</label>
-                        <input type="text" id="fname" placeholder="First Name" className="inp" onBlur={this.validate}/>
-                        {this.state.fn_err ? <div className="error">First Name is required</div> : null}
-                    </div>
+                        <div className="input-section">
+                            <label htmlFor="fname">First Name</label>
+                            <input type="text" id="fname" className="inp"
+                                   onBlur={this.validate} onChange={this.validate}/>
+                            {this.state.validation.fn_err ? <div className="error">First Name is required</div> : null}
+                        </div>
 
-                    <div className="input-section">
-                        <label htmlFor="lname">Last Name</label>
-                        <input type="text" id="lname" placeholder="Last Name" className="inp" onBlur={this.validate}/>
-                        {this.state.ln_err ? <div className="error">Last Name is required</div>: null}
-                    </div>
+                        <div className="input-section">
+                            <label htmlFor="lname">Last Name</label>
+                            <input type="text" id="lname" className="inp"
+                                   onBlur={this.validate} onChange={this.validate}/>
+                            {this.state.validation.ln_err ? <div className="error">Last Name is required</div>: null}
+                        </div>
 
-                    <div className="input-section">
-                        <label htmlFor="dob">Date of Birth</label>
-                        <input type="date" id="dob" className="inp" onBlur={this.validate}/>
-                        {this.state.dob_err ? <div className="error">Date of Birth is required</div>: null}
-                    </div>
+                        <div className="input-section">
+                            <label htmlFor="dob">Date of Birth</label>
+                            <input type="date" id="dob" className="inp"
+                                   onBlur={this.validate} onChange={this.validate}/>
+                            {this.state.validation.dob_err ? <div className="error">Date of Birth is required</div>: null}
+                        </div>
 
-                    <div className="input-section">
-                        <label htmlFor="pass">Password</label>
-                        <input type="password" id="pass" placeholder="Password" className="inp" onBlur={this.validate}
-                               onChange={this.validate}/>
-                        {this.state.pass_err ? <div className="error">Password is required</div> : null}
-                    </div>
+                        <div className="input-section">
+                            <label htmlFor="pass">Password</label>
+                            <input type="password" id="pass" className="inp"
+                                   onBlur={this.validate} onChange={this.validate}/>
+                            {this.state.validation.pass_err ? <div className="error">Password is required</div> : null}
+                        </div>
 
+                        <div className="input-section">
+                            <label htmlFor="cpass">Confirm Password</label>
+                            <input type="password" id="cpass" className="inp"
+                                   onBlur={this.validate} onChange={this.validate}/>
+                            {this.state.validation.cp_err ? <div className="error">Password does not match</div> : null}
+                        </div>
+                    </form>
                     <div className="input-section">
-                        <label htmlFor="cpass">Confirm Password</label>
-                        <input type="password" id="cpass" placeholder="Confirm Password" className="inp"
-                               onBlur={this.validate} onChange={this.validate}/>
-                        {this.state.cp_err ? <div className="error">Password does not match</div> : null}
+                        {this.state.error ? <div className="submitError">Please correct the errors!!</div> : null}
+                        {this.state.filled ? <div className="submitError">Please fill the form!!</div> : null}
+                        <button className="submit" onClick={this.register}>Register</button>
                     </div>
-                </form>
-                <button className="submit" onClick={this.register}>Register</button>
+                    <div className="redirect">
+                        <span>Already a User? </span>
+                        <Link className="linkLogin" to={"/login"}> Login</Link>
+                    </div>
+                </div>
             </div>
         );
     }
